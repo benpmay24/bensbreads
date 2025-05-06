9fb9384a41b0f9dc72fbaab5a5d2a27b45b1df69
Benjamin May <benmay@tesla.com>
2025-05-05 22:46:30 -0700

    moving database to media

--- bensbreads/settings.py
+++ bensbreads/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'bensbreads.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "media"/'db.sqlite3',
     }
 }
 
