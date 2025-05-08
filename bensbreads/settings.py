60b78923cb1dc7c5da62ca486fbde0240c03ebaf
Benjamin May <benmay@tesla.com>
2025-05-07 23:00:43 -0700

    Removing the urls thingy

--- bensbreads/settings.py
+++ bensbreads/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
